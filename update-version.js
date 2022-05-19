b186c8a902e274eec749e8a6a65122b927f642c6
ihor.kyryliuk <ihor.kyryliuk@rightinformation.com>
2022-05-19 13:02:36 +0200

    updater

---
+++ update-version.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+let packageFile = require('./package.json')
+const {exec} = require("child_process");
+packageFile.versionCode++
+
+const updateVersion = () => {
+    let versionUpdateType = process.argv.slice(2)[0]
+    if (versionUpdateType === undefined) {
+        exec('yarn version --patch')
+    } else if (versionUpdateType === 'minor') {
+        exec('yarn version --minor')
+    } else if (versionUpdateType === 'major') {
+        exec('yarn version --major')
+    }
+}
+
+fs.writeFile('./package.json', JSON.stringify(packageFile), (err) => {
+    if (err) {
+        console.error(err)
+        return;
+    }
+    updateVersion();
+})
